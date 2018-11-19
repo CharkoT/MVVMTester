1271f731b6a3f642a49100c9f7919326872821c8
Charko <eeodyd@naver.com>
2018-11-19 22:55:20 +0900

    ViewModel 기능 추가,
    mainActivity <- 디바이스 이미지 갖고 오기
    
    by Charko

--- app/src/main/java/com/charko/tester/mvvmtester/simplemodel/Picture.java
+++ app/src/main/java/com/charko/tester/mvvmtester/simplemodel/Picture.java
@@ -1,17 +1,18 @@
 package com.charko.tester.mvvmtester.simplemodel;
 
-import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Picture {
+public class Picture implements Parcelable {
 
-    private Uri uri;
+    private String uri;
     private String filename;
     private String Location;
     private double latitude;
     private double longitude;
     private String desc;
 
-    public Picture(Uri uri, String filename, String location, double latitude, double longitude, String desc) {
+    public Picture(String uri, String filename, String location, double latitude, double longitude, String desc) {
         this.uri = uri;
         this.filename = filename;
         Location = location;
@@ -20,11 +21,32 @@ public class Picture {
         this.desc = desc;
     }
 
-    public Uri getUri() {
+    protected Picture(Parcel in) {
+        uri = in.readString();
+        filename = in.readString();
+        Location = in.readString();
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+        desc = in.readString();
+    }
+
+    public static final Creator<Picture> CREATOR = new Creator<Picture>() {
+        @Override
+        public Picture createFromParcel(Parcel in) {
+            return new Picture(in);
+        }
+
+        @Override
+        public Picture[] newArray(int size) {
+            return new Picture[size];
+        }
+    };
+
+    public String getUri() {
         return uri;
     }
 
-    public void setUri(Uri uri) {
+    public void setUri(String uri) {
         this.uri = uri;
     }
 
@@ -66,5 +88,20 @@ public class Picture {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(uri);
+        parcel.writeString(filename);
+        parcel.writeString(Location);
+        parcel.writeDouble(latitude);
+        parcel.writeDouble(longitude);
+        parcel.writeString(desc);
     }
 }
